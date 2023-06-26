d4427ba93a7352fb969c7ec031c1d33ada7da5aa
Ben Booth <benwbooth@gmail.com>
2023-06-26 09:17:17 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,64 @@
 use bevy::{prelude::*, window::WindowResized};
 use bevy_ascii_terminal::prelude::*;
+use bevy_common_assets::toml::TomlAssetPlugin;
+use linked_hash_map::LinkedHashMap;
+
+pub const BOARD_WIDTH: usize = 40;
+pub const BOARD_HEIGHT: usize = 25;
+
+#[derive(serde::Deserialize)]
+enum Effect {
+    None,
+}
+
+#[derive(serde::Deserialize)]
+struct Item {
+    name: String,
+    description: String,
+    symbol: String,
+    effect: Effect,
+    x: u16,
+    y: u16,
+}
+
+#[derive(serde::Deserialize)]
+struct Monster {
+    name: String,
+    description: String,
+    symbol: String,
+    hp: u16,
+    att: u16,
+    def: u16,
+}
+
+#[derive(serde::Deserialize)]
+struct Room {
+    description: String,
+    board: String,
+    north: String,
+    south: String,
+    east: String,
+    west: String,
+    items: Vec<Item>,
+    monsters: Vec<Monster>,
+}
+
+
+#[derive(serde::Deserialize, bevy::reflect::TypeUuid)]
+#[uuid = "9cdb0933-7a78-46aa-9075-538223c0882d"]
+struct Castle {
+    rooms: LinkedHashMap<String,Room>,
+}
+
+#[derive(Component)]
+pub struct Location{
+    x: u16, 
+    y: u16,
+}
 
 fn setup(mut commands: Commands) {
     // Create the terminal
-    let mut terminal = Terminal::new([40,25]);
+    let mut terminal = Terminal::new([BOARD_WIDTH,BOARD_HEIGHT]);
     // Draw a blue "Hello world!" to the terminal
     terminal.put_string([0, 24], "Hello world!".fg(Color::BLUE));
 
@@ -22,8 +77,28 @@ fn window_resize_system(resize_event: Res<Events<WindowResized>>) {
     }
 }
 
+fn player_move(
+    mut commands: Commands,
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>) 
+{
+    if input.just_pressed(KeyCode::Up) || input.just_pressed(KeyCode::W) {
+
+    }
+    else if input.just_pressed(KeyCode::Down) || input.just_pressed(KeyCode::S) {
+
+    }
+    else if input.just_pressed(KeyCode::Left) || input.just_pressed(KeyCode::A) {
+
+    }
+    else if input.just_pressed(KeyCode::Right) || input.just_pressed(KeyCode::D) {
+
+    }
+}
+
 fn main () {
     App::new()
+    .add_plugin(TomlAssetPlugin::<Castle>::new(&["castle.toml"]))
     .add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             title: "Castle".into(),
@@ -34,7 +109,11 @@ fn main () {
     }))
     .add_plugin(TerminalPlugin)
     .add_startup_system(setup)
-    .add_system(window_resize_system)
     .insert_resource(ClearColor(Color::BLACK))
+    .add_system(window_resize_system)
+    .add_system(player_move)
+    .add_system(enter_room)
+    .add_system(monster_move)
+    .add_system(render)
     .run();
 }
